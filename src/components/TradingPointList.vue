73bebc9291b1d8b6ca54f56c43e811af4a037cb4
fatalistix <slavabal11@outlook.com>
2024-05-05 13:15:57 +0700

    fixed open bug

--- src/components/TradingPointList.vue
+++ src/components/TradingPointList.vue
@@ -1,36 +1,37 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue';
-import { TradingPoint } from '../models/trading_point';
+import { onMounted, reactive, ref } from 'vue';
 import { tradingPointService } from '../services/trading_point'
+import { TradingPoint } from '../models/trading_point';
 
 const tradingPoints = ref<TradingPoint[]>([])
 
-await tradingPointService.list().then(
+const getTradingPointList = () => {
+  tradingPointService.list().then(
     s => {
-        if (s === null) {
-            alert("TODO: handle null")
-        }
-        tradingPoints.value = s
+      if (s === null) {
+        alert("TODO: handle null")
+      }
+      tradingPoints.value = s
     },
     err => {
-        alert("TODO: handle err: "+err)
+      alert("TODO: handle err: "+err)
     }
-)
+  )
+} 
 
-const count = ref(0)
+getTradingPointList()
 
 </script>
 
 <template>
-  <button @click="count++">{{ count }}</button>
-    <h1>Список торговых точек</h1>
-    <div>
-      <v-list>
-        <v-list-item v-for="(item, i) in tradingPoints" :key="i">
-          {{ item.id+" "+item.type+" "+item.areaPlot+" "+item.rentalCharge+" "+item.counterCount+" "+item.address }}
-        </v-list-item>
-      </v-list>
-    </div>
+  <h1>Список торговых точек</h1>
+  <div>
+    <ul>
+      <li v-for="tradingPoint in tradingPoints" :key="tradingPoint.id">
+        {{ tradingPoint.id+" "+tradingPoint.type+" "+tradingPoint.address }}
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
