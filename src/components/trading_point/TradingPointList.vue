92cebbb4a1b077fbda0a91542045b71cff9b6d98
fatalistix <slavabal11@outlook.com>
2024-05-16 17:06:29 +0700

    moved from components to specified dir

---
+++ src/components/trading_point/TradingPointList.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { useTradingPointStore } from '../../stores/TradingPointStore';
+
+const store = useTradingPointStore()
+store.updateList()
+
+</script>
+
+<template>
+  <h2 class="title is-2 has-text-centered">
+    Список торговых точек
+  </h2>
+  <div class="box">
+    <ul>
+      <li v-for="tradingPoint in store.tradingPointList" :key="tradingPoint.id">
+        {{ tradingPoint.id+" "+tradingPoint.type+" "+tradingPoint.address }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
