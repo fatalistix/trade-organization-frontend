60b668418f08cda5f8d6f87b7a2ce4ac0c7d905e
fatalistix <slavabal11@outlook.com>
2024-05-16 20:16:11 +0700

    added register actionc

--- src/stores/TradingPointStore.ts
+++ src/stores/TradingPointStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from 'pinia'
-import { TradingPoint } from '../models/trading_point'
+import { TradingPoint, TradingPointType } from '../models/trading_point'
 import { TradingPointService } from '../services/trading_point'
+import { Money } from '../models/core'
 
 interface State {
     tradingPointList: TradingPoint[]
@@ -23,10 +24,31 @@ export const useTradingPointStore = defineStore('TradingPointStore', {
                         resolve()
                     },
                     e => {
-                        reject(e)    
+                        reject(e)
                     }
                 )
             })
         },
+        async register(
+            t: TradingPointType,
+            areaPlot: number,
+            rentalCharge: Money,
+            counterCount: number,
+            address: string
+        ): Promise<void> {
+            return new Promise((resolve, reject) => {
+                tradingPointService.register(
+                    t, areaPlot, rentalCharge, counterCount, address
+                ).then(
+                    _ => {
+                        resolve()
+                    },
+                    e => {
+                        reject(e)
+                    }
+                )
+                this.updateList()
+            })
+        }
     }
 })
