7c1946dce16f36e68fd5522317075c4792f8e4c7
fatalistix <slavabal11@outlook.com>
2024-05-16 17:06:04 +0700

    page for editing trading points as a manager

---
+++ src/components/manager/ManagerTradingPoint.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import TradingPointList from '../trading_point/TradingPointList.vue';
+</script>
+
+<template>
+    <TradingPointList/>
+</template>
+
+<style scoped>
+</style>
