d05bb01ef22cd2c032f22f75ca9660963f5c9c6e
fatalistix <slavabal11@outlook.com>
2024-05-16 17:01:31 +0700

    moved router config to separate file

--- src/main.ts
+++ src/main.ts
@@ -1,39 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createRouter, createWebHistory } from 'vue-router'
-
-import TradingPointRegister from './components/TradingPointRegister.vue'
-import SellerRegister from './components/SellerRegister.vue'
-import SellerList from './components/SellerList.vue'
-import TradingPointList from './components/TradingPointList.vue'
+import { router } from './router'
 
 import App from './App.vue'
 
 import './app.scss'
 
 const pinia = createPinia()
+
 const app = createApp(App)
-const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
-    routes: [
-        {
-            path: '/manager/trading-points/',
-            component: TradingPointList
-        },
-        {
-            path: '/manager/trading-points/register',
-            component: TradingPointRegister
-        },
-        {
-            path: '/staff',
-            component: SellerRegister
-        },
-        {
-            path: '/trading-point/:trading_point_id/:trading_point_type/sellers',
-            component: SellerList
-        },
-    ]
-})
 
 app.use(pinia)
 app.use(router)
