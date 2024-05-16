599bfe385cc9617a7217872c16a9453696315c18
fatalistix <slavabal11@outlook.com>
2024-05-16 17:35:15 +0700

    added notifications lib

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
+import { Notifications } from '@kyvg/vue3-notification';
 import Navbar from './components/Navbar.vue';
 </script>
 
 <template>
   <Navbar />
   <RouterView />
+  <Notifications />
 </template>
 
 <style scoped>
