e1d641caeb53753c1ce6867eee7a0393460b9e9d
fatalistix <slavabal11@outlook.com>
2024-05-16 20:14:52 +0700

    added column view

--- src/components/manager/ManagerTradingPoint.vue
+++ src/components/manager/ManagerTradingPoint.vue
@@ -1,10 +1,20 @@
 <script setup lang="ts">
 import TradingPointList from '../trading_point/TradingPointList.vue';
+import TradingPointRegister from '../trading_point/TradingPointRegister.vue';
+
 </script>
 
 <template>
-    <TradingPointList/>
+    <div class="container">
+        <div class="columns">
+            <div class="column">
+                <TradingPointRegister />
+            </div>
+            <div class="column">
+                <TradingPointList />
+            </div>
+        </div>
+    </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
