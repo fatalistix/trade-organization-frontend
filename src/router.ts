62926b5b48b4796ff13e2f7df63aaf55488e41b3
fatalistix <slavabal11@outlook.com>
2024-05-16 16:57:38 +0700

    separated router to external file

---
+++ src/router.ts
@@ -0,0 +1,44 @@
+import { createRouter, createWebHistory } from "vue-router"
+import { useUserStore, UserType } from "./stores/UserStore"
+
+import Login from "./components/login/Login.vue"
+import ManagerHome from "./components/manager/ManagerHome.vue"
+import ManagerTradingPoint from "./components/manager/ManagerTradingPoint.vue"
+import NotFound from "./components/NotFound.vue"
+
+export const router = createRouter({
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            redirect: '/login'
+        },
+        {
+            path: '/login',
+            component: Login
+        },
+        {
+            path: '/manager',
+            component: ManagerHome
+        },
+        {
+            path: '/manager/trading-point',
+            component: ManagerTradingPoint
+        },
+        {
+            path: '/:pathMatch(.*)*',
+            name: 'NotFound',
+            component: NotFound
+        }
+    ]
+})
+
+router.afterEach((to) => {
+    if (to.path === "/login") {
+        useUserStore().type = UserType.LOGIN
+    } else if (to.path === "/manager") {
+        useUserStore().type = UserType.MANAGER
+    } else if (to.path.startsWith("/trading-point")) {
+        useUserStore().type = UserType.SELLER
+    }
+})
