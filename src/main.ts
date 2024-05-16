fabb1e1cd40c864d7fcd9f718dcfb96448721601
fatalistix <slavabal11@outlook.com>
2024-05-16 17:35:26 +0700

    added notifications lib

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { createPinia } from 'pinia'
 import { router } from './router'
 
 import App from './App.vue'
+import Notifications from '@kyvg/vue3-notification'
 
 import './app.scss'
 
@@ -12,4 +13,5 @@ const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
+app.use(Notifications)
 app.mount("#app")
