c17ba2ff77118b9d68ab9268604f9d81d6842ff3
fatalistix <slavabal11@outlook.com>
2024-06-06 15:59:22 +0700

    section components

---
+++ src/components/section/SectionAdd.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useNotification } from '@kyvg/vue3-notification';
+import { useDepartmentStoreStore } from '../../stores/DepartmentStoreStore';
+
+const departmentStoreStore = useDepartmentStoreStore()
+const notification = useNotification()
+
+function addSection() {
+    departmentStoreStore.addSection().then(
+        _ => {
+            notification.notify({
+                text: "Секция добавлена",
+                type: "success"
+            })
+        },
+        _ => {
+            notification.notify({
+                text: "Не удалось добавить секцию",
+                type: "warn"
+            })
+        }
+    )
+}
+</script>
+
+<template>
+    <div class="field has-text-centered">
+        <button class="button is-primary" @click="addSection()">Добавить</button>
+    </div>
+</template>
+
+<style scoped></style>
---
+++ src/components/section/SectionList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { useDepartmentStoreStore } from '../../stores/DepartmentStoreStore';
+
+const departmentStoreStore = useDepartmentStoreStore()
+
+</script>
+
+<template>
+    <article class="media" v-for="section in departmentStoreStore.sections">
+        <div class="media-content">
+            <strong>Секция {{ section.id }}</strong>
+        </div>
+    </article>
+</template>
+
+<style scoped>
+</style>
