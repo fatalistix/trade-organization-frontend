72c1d53be7f4075ebb9c77ca42a25cc0553d6143
fatalistix <slavabal11@outlook.com>
2024-06-10 20:00:29 +0700

    updated routes

--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,18 @@
 import { createRouter, createWebHistory } from "vue-router"
-import { useUserStore, UserType } from "./stores/UserStore"
 
 import Login from "./components/login/Login.vue"
 import ManagerHome from "./components/manager/ManagerHome.vue"
 import ManagerTradingPoint from "./components/manager/ManagerTradingPoint.vue"
 import ManagerSeller from "./components/manager/ManagerSeller.vue"
 import NotFound from "./components/NotFound.vue"
+import ManagerDepartmentStore from "./components/manager/ManagerDepartmentStore.vue"
+import TradingPointHome from "./components/trading_point/TradingPointHome.vue"
+import SellerHome from "./components/seller/SellerHome.vue"
+import ManagerProduct from "./components/manager/ManagerProduct.vue"
+import ManagerSupplier from "./components/manager/ManagerSupplier.vue"
+import ManagerSupplierEdit from "./components/manager/ManagerSupplierEdit.vue"
+import ManagerCustomer from "./components/manager/ManagerCustomer.vue"
+import ManagerOrder from "./components/manager/ManagerOrder.vue"
 
 export const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -23,12 +30,44 @@ export const router = createRouter({
             component: ManagerHome
         },
         {
-            path: '/manager/trading-point',
+            path: '/manager/trading-points',
             component: ManagerTradingPoint
         },
         {
-            path: '/manager/seller',
+            path: '/manager/trading-point/:id/department_store',
+            component: ManagerDepartmentStore
+        },
+        {
+            path: '/manager/products',
+            component: ManagerProduct
+        },
+        {
+            path: '/manager/sellers',
             component: ManagerSeller,
+        },
+        {
+            path: '/manager/suppliers',
+            component: ManagerSupplier,
+        },
+        {
+            path: '/manager/supplier/:id',
+            component: ManagerSupplierEdit,
+        },
+        {
+            path: '/manager/customers',
+            component: ManagerCustomer,
+        },
+        {
+            path: '/manager/orders',
+            component: ManagerOrder,
+        },
+        {
+            path: '/trading-point/:id/:type',
+            component: TradingPointHome,
+        },
+        {
+            path: '/seller/:id',
+            component: SellerHome,
         },
         {
             path: '/:pathMatch(.*)*',
@@ -36,14 +75,4 @@ export const router = createRouter({
             component: NotFound
         }
     ]
-})
-
-router.afterEach((to) => {
-    if (to.path === "/login") {
-        useUserStore().type = UserType.LOGIN
-    } else if (to.path === "/manager") {
-        useUserStore().type = UserType.MANAGER
-    } else if (to.path.startsWith("/trading-point")) {
-        useUserStore().type = UserType.SELLER
-    }
 })
