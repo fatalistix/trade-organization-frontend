6465cd44e30ef3a17e208b2849988ef7b488608c
fatalistix <slavabal11@outlook.com>
2024-06-10 19:59:20 +0700

    reorganized structure

--- src/stores/DepartmentStoreStore.ts
+++ src/stores/DepartmentStoreStore.ts
@@ -1,26 +1,49 @@
 import { defineStore } from "pinia";
 import { DepartmentStore } from "../models/department_store";
-import { Hall } from "../models/hall";
-import { Section } from "../models/section";
+import { TradingPointService } from "../services/tradingpoint/tradingpoint";
 import { HallContainerType } from "../models/hall_container";
 import { TradingPointType } from "../models/trading_point";
 
 interface State {
     departmentStore: DepartmentStore | null
-    sections: Section[]
-    halls: Hall[]
 }
+
+const service = new TradingPointService()
 
 export const useDepartmentStoreStore = defineStore('DepartmentStoreStore', {
     state: (): State => ({
         departmentStore: null,
-        sections: [],
-        halls: []
     }),
     getters: {
 
     },
     actions: {
-
+        load(id: number): Promise<void> {
+            return new Promise((resolve, reject) => {
+                service.departmentStore(id).then(
+                    departmentStore => {
+                        this.departmentStore = departmentStore
+                        resolve()
+                    },
+                    err => reject(err)
+                )
+            })   
+        },
+        addHall(sectionId: number): Promise<void> {
+            if (!this.departmentStore) {
+                return new Promise((_, reject) => reject("no department store"))
+            } else {
+                return service.addHall(
+                    sectionId, HallContainerType.SECTION, this.departmentStore.id, TradingPointType.DEPARTMENT_STORE
+                )
+            }
+        },
+        addSection(): Promise<number> {
+            if (!this.departmentStore) {
+                return new Promise((_, reject) => reject("no department store"))
+            } else {
+                return service.addSection(this.departmentStore.id)
+            }
+        },
     }
 })
--- src/stores/ProductStore.ts
+++ src/stores/ProductStore.ts
@@ -1,18 +1,46 @@
 import { defineStore } from "pinia";
 import { Product } from "../models/product";
+import { ProductService } from "../services/product/product";
 
 interface State {
     products: Product[]
 }
+
+const service = new ProductService()
 
 export const useProductStore = defineStore("ProductStore", {
     state: (): State => ({
         products: []
     }),
     getters: {
-
+        getById: (state: State) => {
+            return (id: number) => state.products.find(product => product.id === id)
+        },
     },
     actions: {
-        
+        create(name: string, description: string): Promise<number> {
+            return service.create(name, description)
+        },
+        updateProducts(): Promise<void> {
+            return new Promise((resolve, reject) => {
+                service.list().then(
+                    products => {
+                        this.products = products
+                        resolve()
+                    },
+                    err => reject(err)
+                )
+            })
+        },
+        product(id: number): Promise<Product> {
+            return service.product(id)
+        },
+        update(
+            id: number,
+            name: string,
+            description: string,
+        ): Promise<number> {
+            return service.update(id, name, description)
+        }
     }
 })
